a5c4ec79593adced3333348f7b0dfea2808dace0
Spivee <spiveehere@gmail.com>
2019-08-15 10:47:51 +1000

    add centres to caps
    makes weird ass lighting less weird

--- T3D/T3D/Cylinder.cpp
+++ T3D/T3D/Cylinder.cpp
@@ -5,13 +5,15 @@ namespace T3D {
 	Cylinder::Cylinder(float radius, float height, int sides)
 	{
 		// Init vertex and index arrays
-		initArrays(2 * sides,	// num vertices
+		initArrays(2 + 2 * sides,	// num vertices
 			2 * sides,		// num tris
 			sides);		// num quads
 
 					// Set vertices
 
 		float dtheta = 2 * 3.1415926535 / float(sides);
+		setVertex(2 * sides, 0, height, 0);
+		setVertex(2 * sides + 1, 0, -height, 0);
 		for (int i = 0; i < sides; i++) {
 			float theta = dtheta * i;
 			float x = radius * cos(theta);
@@ -25,8 +27,8 @@ namespace T3D {
 			// int j = (i - 1) % sides;
 			setFace(i, i, sides + i, sides + j, j);
 
-			setFace(i, 0, i, j);
-			setFace(sides + i, sides + 0, sides + j, sides + i);
+			setFace(i, 2 * sides, i, j);
+			setFace(sides + i, 2 * sides + 1, sides + j, sides + i);
 		}
 
 
